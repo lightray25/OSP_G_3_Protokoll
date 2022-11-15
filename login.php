ce767f45e74cc2f57e2f01e9e3c6cec8f38001b3
daniel-puetzstueck <118170566+daniel-puetzstueck@users.noreply.github.com>
2022-11-15 10:46:46 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+include 'header.html'; ?>
+            <div id="content">
+                <div class="Formbericht">
+                    <div class="login">    
+                        <form id="login" method="get" action="login.php">  
+                            <label><b>User Name</b></label>    
+                            <input type="text" name="Uname" id="Uname" placeholder="Username">    
+                            <br><br>    
+                            <label><b>Password</b></label>    
+                            <input type="Password" name="Pass" id="Pass" placeholder="Password">    
+                            <br><br>    
+                            <input type="button" class="btn btn-primary" name="log" id="log" value="Log In Here">       
+                            <br><br>    
+                            <input type="checkbox" id="check">    
+                            <span>Remember me</span>  
+                            <br><br>  
+                            <a href="#">Forgot Password?</a>    
+                        </form>
+                    </div>     
+                </div>    
+            </div>
+<?php
+include 'footer.html'; ?>
---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+include 'header.html'; ?>
+            <div id="content">
+            <p><object data="vorlage.html" width="700" height="800">
+            Ihr Browser kann das Objekt leider nicht anzeigen!
+            </object></p>
+            </div>
+<?php
+include 'footer.html'; ?>
