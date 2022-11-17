32a7ca595b1b32e7ace0b3694e8cdc9ada9dc70b
Projektleiter <113103717+Projektleiter@users.noreply.github.com>
2022-11-17 11:06:51 +0100

    Update Login.php
    Rufen der main.php bei drücken des Loginbuttons
    Anpassungen Header auf der Loginseite

--- login.php
+++ login.php
@@ -3,14 +3,14 @@ include 'header.html'; ?>
             <div id="content">
                 <div class="Formbericht">
                     <div class="login">    
-                        <form id="login" method="post" action="login.php">  
+                        <form id="login" method="post" action="main.php">  
                             <label><b>User Name</b></label>    
                             <input type="text" name="Uname" id="Uname" placeholder="Username">    
                             <br><br>    
                             <label><b>Password</b></label>    
                             <input type="Password" name="Pass" id="Pass" placeholder="Password">    
                             <br><br>    
-                            <input type="button" class="btn btn-primary" name="log" id="log" value="Log In Here">       
+                            <input type="submit" class="btn btn-primary" name="log" id="log" value="Log In Here">       
                             <br><br>    
                             <input type="checkbox" id="check">    
                             <span>Remember me</span>  
@@ -22,3 +22,25 @@ include 'header.html'; ?>
             </div>
 <?php
 include 'footer.html'; ?>
+
+<script type="text/javascript">
+    document.getElementById("schriftLogin").style.display = "none"; // Schrift oben links auf Login unsichtbar
+    document.getElementById("greetingName").innerHTML = 'Sharepoint Protokolle'
+
+    document.getElementById("log").onclick = function(){
+        if (document.getElementById("Uname").value == "Schulleitung")
+            if (document.getElementById("Pass").value == "123")
+                console.log("Etwas")
+            // href zu dashboard.php
+
+        if (document.getElementById("Uname").value == "Lehrkraft1")
+            if (document.getElementById("Pass").value == "123")
+                console.log("Etwas")
+            // href zu dashboard.php
+
+        if (document.getElementById("Uname").value == "Lehrkraft2")
+            if (document.getElementById("Pass").value == "123")
+            console.log("Etwas")
+            // href zu dashboard.php
+    }
+</script>
